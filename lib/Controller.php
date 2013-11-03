fd01d26caab34ad9d5136ac0c17d31089b06e965
vadym <radwwmail@gmail.com>
2013-11-03 14:54:12 +0200

    Update Controller.php
    Parsedown is in different namespace so adding backslash

--- lib/Controller.php
+++ lib/Controller.php
@@ -13,7 +13,7 @@ class Controller extends \AbstractController {
 
         $page = $this->api->page_object = $this->target->add('Page');
 
-        $html = Parsedown::instance()->parse(file_get_contents($path));
+        $html = \Parsedown::instance()->parse(file_get_contents($path));
 
         $page->template->loadTemplateFromString($html);
 
