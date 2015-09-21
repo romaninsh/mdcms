b16d34223cf7f4c6400e4443549feda9869a282f
Romans Malinovskis <romans@agiletoolkit.org>
2015-09-21 21:17:28 +0100

    remove title if page has it

--- lib/Page.php
+++ lib/Page.php
@@ -2,12 +2,13 @@
 namespace romaninsh\mdcms;
 class Page extends \Page {
 
-    protected $view_class='romaninsh/mdcms/View'; 
-    protected $controller_class='romaninsh/mdcms/View'; 
+    protected $view_class='romaninsh/mdcms/View';
+    protected $controller_class='romaninsh/mdcms/View';
 
 
     function init(){
         parent::init();
+        $this->app->layout->template->tryDel('has_page_title');
         $c=$this->api->add('romaninsh/mdcms/Controller',array('init'=>false)); // in case it's not there
         $v=$this->add($this->view_class);
         $page = str_replace('_','/',$this->api->page);
